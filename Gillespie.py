9140d5845f2922703a919fb5fcba42b49946967b
Herman Gudjonson <gyftdresaw@gmail.com>
2013-07-09 11:33:41 -0500

    birth/death plots added

--- Gillespie.py
+++ Gillespie.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import poisson
 
 class Gillespie:
     def __init__(self,IC):
@@ -32,7 +33,9 @@ class Gillespie:
             while time < t:
                 rates = self.get_rates(data)
                 total_rate = reduce(lambda x,y: x+y,rates)
-                time_step = np.random.exponential(total_rate)
+                # input to exponential is scale = 1/rate
+                time_step = np.random.exponential(1.0/total_rate)
+                # print (time,data,rates,total_rate,time_step)
                 
                 time += time_step
                 self.time_points[i].append(time) # record time
@@ -49,13 +52,23 @@ class Gillespie:
         data = np.empty((self.trials,len(self.IC)))
         for i in xrange(self.trials):
             di = 0
-            while self.time_points[i][di] < t:
+            while self.time_points[i][di] <= t:
                 di += 1
             # add data right before
             data[i,:] = self.data_points[i][di-1]
-        
+        # data is (trials x num_species)
         return data
 
+    # for each time in times 
+    # calculate moment m for each species
+    def get_moment(self,times,m):
+        mvals = np.empty((len(times),len(self.IC)))
+        for i in xrange(len(times)):
+            d = self.get_data(times[i])
+            # calculate moment down the columns
+            mvals[i,:] = np.mean(d ** m, axis = 0)
+        # mvals is (times x num_species)
+        return mvals
 
 # quick test
 def birth(d):
@@ -69,7 +82,7 @@ def birth_rate(d,kb):
 def death(d):
     x = d[:]
     if x[0] == 0:
-        return d
+        return x
     else:
         x[0] -= 1
         return x
@@ -78,15 +91,52 @@ def death_rate(d,kd):
     return kd * d[0]
 
 G = Gillespie([0])
-KB = 1.2
-KD = 1.0
+KB = 2.0
+KD = 0.5
+T = 1000
 G.add_reaction(birth,lambda x: birth_rate(x,KB))
 G.add_reaction(death,lambda x: death_rate(x,KD))
-G.simulate(100.0,1000)
-results = G.get_data(100.0)
+G.simulate(10,T)
 
-hist, bins = np.histogram(results,bins = 20)
-width = 0.7*(bins[1]-bins[0])
-center = (bins[:-1]+bins[1:])/2
-plt.bar(center, hist, align = 'center', width = width)
+# a few plots
+# distribution at a few times
+times = np.linspace(0.1,3,7)
+for_hist = np.empty((T,len(times)))
+for i in xrange(len(times)):
+    results = G.get_data(times[i])
+    for_hist[:,i] = results[:,0]
+
+plt.figure()
+n,bins,patches = plt.hist(for_hist,bins=range(10),align='left',normed=True)
+# add expected poisson distribution lines
+# disregard time 0, distribution at that time doesn't make sense
+poiss_lines = np.empty((len(bins),len(times)))
+for i in xrange(len(times)):
+    mu_expected = (KB/KD) * (1.0 - np.exp(-KD * times[i]))
+    poiss_lines[:,i] = poisson.pmf(bins,mu_expected)
+plt.plot(bins,poiss_lines)
+plt.legend(times)
+plt.title('distribution in time')
 plt.show()
+
+# plot mean in time
+t = np.linspace(0,10,100)
+mean_data = G.get_moment(t,1)
+plt.figure()
+plt.plot(t,mean_data)
+mean_exp = (KB/KD) * (1.0 - np.exp(-KD * t))
+plt.plot(t,mean_exp,'--')
+plt.title('mean in time')
+plt.show()
+
+# plot variance in time
+var_data = G.get_moment(t,2) - (mean_data * mean_data)
+plt.figure()
+plt.plot(t,var_data)
+plt.plot(t,mean_exp,'--')
+plt.title('var in time')
+plt.show()
+
+
+
+
